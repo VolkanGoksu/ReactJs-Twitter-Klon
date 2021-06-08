a830faba5e183dec902f535beb1d5ac616991ee9
volkanGoksu <volkan.goksu@outlook.com>
2021-06-08 16:07:14 +0300

    npm tailwindcss and npm i and starter

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+
+import React from 'react'
+function App() {
+  return (
+    <div className="App">
+       <h2 className="bg-purple-500">Hello World!</h2>
+    </div>
+  );
+}
+
+export default App;
