9e38b01061e7fb6f328a8cd74fed66df7c8d75eb
volkanGoksu <volkan.goksu@outlook.com>
2021-06-09 01:35:32 +0300

    sidebar icons

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Container from './layout/Container'
 import SideBar from './layout/SideBar'
 import Content from './layout/Content'
 import Widgets from './layout/Widgets'
+
 function App() {
   return (
     <div className="App">
--- client/src/layout/SideBar.js
+++ client/src/layout/SideBar.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
+import { HomeIcon } from '../icons/Icon'
+import { NotificationsIcon } from '../icons/Icon'
+import { MessagesIcon } from '../icons/Icon'
+import { BookmarksIcon } from '../icons/Icon'
+import { ListsIcon } from '../icons/Icon'
+import { ProfileIcon } from '../icons/Icon'
+import { MoreIcon } from '../icons/Icon'
+import { ExploreIcon } from '../icons/Icon'
+
 const SideBar = () => {
     return (
         <div className="w-72 bg-blue-300">
-             <h2>SideBar</h2>
+            <h2>SideBar</h2>
+            <HomeIcon />
+            <ExploreIcon />
+            <NotificationsIcon/>
+            <MessagesIcon />
+            <BookmarksIcon/>
+            <ListsIcon/>
+            <ProfileIcon/>
+            <MoreIcon />
+
+
         </div>
     )
 }
