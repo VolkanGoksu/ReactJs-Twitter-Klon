94ab4fa7b9c498333fa20b8666dfe7d881e6b313
volkanGoksu <volkan.goksu@outlook.com>
2021-06-09 22:58:44 +0300

    install and using react twitter widget

--- client/src/layout/Widgets.js
+++ client/src/layout/Widgets.js
@@ -1,15 +1,25 @@
 import React from 'react'
-import {SearchIcon} from '../icons/Icon'
+import { Timeline } from 'react-twitter-widgets'
+
+import { SearchIcon } from '../icons/Icon'
 const Widgets = () => {
     return (
         <aside >
             <div className="flex items-center space-x-2 p-3 m-3 bg-gray-200 rounded-full text-gray-dark focus-within:bg-white focus-within:ring-1 focus-within:ring-primary-base focus-within:text-primary-base">
-                <SearchIcon className="w-5 h-5"/>
+                <SearchIcon className="w-5 h-5" />
                 <input type="text" placeholder="Search Twitter" className="placeholder-gray-dark bg-transparent focus:outline-none w-full text-sm "></input>
             </div>
-           <div>
-               TimeLine
-           </div>
+            <div className="mt-4">
+                <Timeline
+                    dataSource={{
+                        sourceType: 'profile',
+                        screenName: 'reactjs'
+                    }}
+                    options={{
+                        height: '1000'
+                    }}
+                />
+            </div>
         </aside>
     )
 }
