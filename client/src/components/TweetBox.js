43d4276c7af167a7bb46c6ffac5d07321085925f
volkanGoksu <volkan.goksu@outlook.com>
2021-06-10 22:33:31 +0300

    content setcontent and create firestore tweet collection

--- client/src/components/TweetBox.js
+++ client/src/components/TweetBox.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { EmojiIcon, GIFIcon, ImageIcon, PollIcon, ScheduleIcon } from '../icons/Icon'
+import db from '../firebase'
+import firebase from 'firebase'
 const TweetBox = () => {
+    const [content, setContent] = useState('')
+    const addTweet = () => {
+        if (content !== '') {
+            db.collection('tweet').add({
+                displayName: "Volkan Göksu",
+                userName: '@Volkngoksu',
+                content:content,
+                timeStap:firebase.firestore.FieldValue.serverTimestamp(),
+                avatar:"https://pbs.twimg.com/profile_images/1380420418327171072/xujU9k82_400x400.jpg"  
+             })
+             setContent('')
+        }
+    }
     return (
         <div className="flex flex-col flex-1 mt-2 px-2">
-            <textarea className="w-full text-lg placeholder-gray-dark outline-none overflow-hidden resize-none bg-transparent " placeholder="Neler oluyor ?" />
+            <textarea className="w-full text-lg placeholder-gray-dark outline-none overflow-hidden resize-none bg-transparent "
+                placeholder="Neler oluyor ?"
+                onChange={e => setContent(e.target.value)}
+                value={content}
+            />
             <div className="flex items-center justify-between">
                 <div className="flex -ml-3">
                     <div className="flex items-center justify-center w-11 h-11 rounded-full hover:bg-gray-lightest">
@@ -25,8 +44,8 @@ const TweetBox = () => {
                         <EmojiIcon className="w-6 h-6 text-primary-base" />
                     </div>
                 </div>
-                
-                <button className="bg-primary-base text-white rounded-full px-4 py-2 ">Tweet</button>
+
+                <button onClick={addTweet} className="bg-primary-base text-white rounded-full px-4 py-2 ">Tweet</button>
             </div>
         </div>
     )
